62b5c5f67f155798257e9458f1680d7110808ef8
Sadra Amlashi <amlashi.sadra@gmail.com>
2020-12-22 14:25:57 +0330

    add github repos

---
+++ src/components/github-link.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const GithubLink = ({ name, description, stars, forks, link, thumbnail }) => (
+  <article className="card ">
+    <a href={link}>
+      {!!thumbnail && (
+        <img src={thumbnail} alt={"- Featured Shot"} />
+      )}
+    </a>
+    <header>
+      <h2 className="post-title">
+        <a href={link} className="post-link">
+          {name}
+        </a>
+      </h2>
+      <div className="post-meta">{description}</div>
+      <div>
+        <i class="far fa-star"></i> {stars}
+        <span>    </span>
+        <i class="fas fa-code-branch"></i> {forks}
+      </div>
+    </header>
+  </article>
+)
+export default GithubLink
